ce1e98899211cc22d7b8418369fe36866223739c
Shivaprasad-2000 <119153034+Shivaprasad-2000@users.noreply.github.com>
2022-11-27 16:39:09 +0530

    Update Bill_Split_by_array.cpp

--- Bill_Split_by_array.cpp
+++ Bill_Split_by_array.cpp
@@ -18,7 +18,7 @@ void Input(int *n , Instances* &I)
 		cin>>temp_amount;
 		cout<<"Enter why amount is spent : ";
 		cin>>I->Reason;
-		cout<<"Enter who paid the amount \n (me) for you \n (him) for him : "<<endl;
+		cout<<"Enter who paid the amount \n(me) for you \n(him) for him : "<<endl;
 		cin>>I->Who_paid;
 		if(I->Who_paid=="him")
 			I->Amount = -1*temp_amount;
@@ -33,20 +33,22 @@ void Output(int n ,Instances* &I_temp)
 	float Due;
 	for(int i=0;i<n;i++)
 	{
-		
-		cout<<"Amount spent = "<<I_temp->Amount<<" by "<<I_temp->Who_paid<<" for "<<I_temp->Reason<<endl;
+		if (I_temp->Amount < 0)
+			cout<<"Amount spent = "<<(I_temp->Amount*(-1))<<" by "<<I_temp->Who_paid<<" for "<<I_temp->Reason<<endl;
+			
+		else
+			cout<<"Amount spent = "<<I_temp->Amount<<" by "<<I_temp->Who_paid<<" for "<<I_temp->Reason<<endl;
 		Due=Due+(I_temp->Amount/2);
-		
 		I_temp++;
 	}
-	if(Due/(Due*-1)==1)
-		cout<<" He Should Pay you : "<<Due<<endl;
+	if(Due < 0)
+		cout<<"You need to pay him : "<<(Due*(-1))<<endl;
 	else
-		cout<<" You need to pay him : "<<Due<<endl;
+		cout<<"He Should Pay you : "<<Due<<endl;
 }
 int main()
 {
-	int n=0;
+	int n;
 	Instances *I = new Instances[n],*I_temp = I;
 	Input(&n,I);
 	Output(n,I_temp);
